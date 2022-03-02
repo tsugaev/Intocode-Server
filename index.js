a67ced6b823b75ae60abca443ae22371dc058f48
tsugaev <tsuagev@mail.com>
2022-03-02 10:37:59 +0300

    asdas

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const mongoose = require("mongoose");
 const express = require("express");
 
+const app = express();
+app.use(express.json());
+
+const a = 10;
+
+if (a > 2) {
+  console.log(21);
+}
+
+mongoose
+  .connect("mongodb+srv://overlord:Edb22edb22@cluster0.e2awg.mongodb.net/Intocode")
+  .then(() => console.log("Подключение к MongoDB успешно"))
+  .catch(() => console.log("Не удалось подключиться"));
+
+app.use(require("./routes"));
+
 app.listen(3000, () => {
   console.log("Сервер запушен");
 });
